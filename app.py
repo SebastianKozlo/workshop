3ff2391277d34f8437f4c361a2000b596ace7403
SebastianKozlo <szustakowski.sebast@gmail.com>
2025-10-01 10:30:55 +0000

    1234

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Global Azure 2025!!!!!!!!!!"
+    return "Slawek Wez sie za robote"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
