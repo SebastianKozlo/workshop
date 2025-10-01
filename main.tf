afbd7019f5e498b7d124d862729fd4b1e38c4750
SebastianKozlo <szustakowski.sebast@gmail.com>
2025-10-01 10:08:00 +0000

    second2

--- main.tf
+++ main.tf
@@ -22,16 +22,16 @@ terraform {
 resource "azurerm_service_plan" "example" {
   name                = "example-app-service-plan-09"
   location            = "westeurope"
-  resource_group_name = "example-resources-09"
+  resource_group_name = "rg-09"
   os_type             = "Linux"
   sku_name            = "P0v3"
 }
 
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "example-webapp-123123i95u8fhwfdsewdwsa" #change here
-  location            = "westeurope" #change here
-  resource_group_name = "example-resources" #change here
+  name                = "example-webapp-123123i95u8fhwfdsewdwsa-09"
+  location            = "westeurope"
+  resource_group_name = "rg-09"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
