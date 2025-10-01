459bb3b14f72f424942867342eebd32caa45e4a6
SebastianKozlo <szustakowski.sebast@gmail.com>
2025-10-01 13:01:02 +0000

    rfrer

--- main.tf
+++ main.tf
@@ -35,3 +35,35 @@ resource "azurerm_linux_web_app" "example" {
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
+
+  resource "azurerm_storage_account" "example_storage" {
+    name                     = "storagea23123234"
+    resource_group_name      = "rg-09"
+    location                 = "westeurope"
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+
+    tags = {
+      environment = "staging"
+    }
+  }
+
+  # MSSQL Server
+  resource "azurerm_mssql_server" "example" {
+    name                         = "workshop-mssql-server"
+    resource_group_name          = "rg-09"
+    location                     = "westeurope"
+    version                      = "12.0"
+    administrator_login          = "sqladminuser"
+    administrator_login_password = "P@ssw0rd1234!"
+    minimum_tls_version          = "1.2"
+  }
+
+  # MSSQL Database
+  resource "azurerm_mssql_database" "example" {
+    name               = "workshopdb"
+    server_id          = azurerm_mssql_server.example.id
+    sku_name           = "S0"
+    collation          = "SQL_Latin1_General_CP1_CI_AS"
+    max_size_gb        = 5
+  }
